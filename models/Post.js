6d20cba85b549df9cb277e1e110e08b3f0832716
hxnshaw <eghosasere07@gmail.com>
2022-07-21 17:07:04 +0100

    Post Schema

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "PLEASE PROVIDE THE NAME OF THE LOCATION"],
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [
+        true,
+        "PLEASE PROVIDE THE DETAILS AND DESCRIPTION OF THE LOCATION",
+      ],
+      minlength: 10,
+      maxlength: 250,
+    },
+    image: {
+      type: String,
+      required: [true, "PLEASE PROVIDE IMAGES FOR THE LOCATION"],
+      default: "/uploads/blogger.jpeg",
+    },
+    recommended: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
